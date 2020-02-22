7cce107db30365c46b81771cc0aa9eecbada2cbd
Dimitrie-Toma Furdui <tomadimitrie@icloud.com>
2020-02-22 16:31:13 +0200

    added tilemap, player movement, wall detection

--- Ancient Hill/GameScene.swift
+++ Ancient Hill/GameScene.swift
@@ -7,83 +7,47 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var player: SKNode? = nil
+    var tileMap: SKTileMapNode? = nil
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        guard let camera = self.childNode(withName: "//camera") as? SKCameraNode else { return }
+        guard let player = self.childNode(withName: "//player") else { return }
+        guard let tileMap = self.childNode(withName: "//tileMap") as? SKTileMapNode else { return }
+        camera.constraints = [.distance(.init(lowerLimit: 0, upperLimit: 0), to: player)]
+        self.player = player
+        self.tileMap = tileMap
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        let touch = touches.first
+        if let position = touch?.location(in: self) {
+            let tappedNode = self.nodes(at: position)
+            if let initialPosition = self.player?.position {
+                var newPosition = CGPoint(x: initialPosition.x, y: initialPosition.y)
+                switch tappedNode.first?.name {
+                    case "top":
+                        newPosition.y += Constants.moveAmount
+                    case "left":
+                        newPosition.x -= Constants.moveAmount
+                    case "right":
+                        newPosition.x += Constants.moveAmount
+                    case "bottom":
+                        newPosition.y -= Constants.moveAmount
+                    default:
+                        break
+                }
+                if let tileColumn = self.tileMap?.tileColumnIndex(fromPosition: newPosition), let tileRow = self.tileMap?.tileRowIndex(fromPosition: newPosition) {
+                    let tile = self.tileMap?.tileDefinition(atColumn: tileColumn, row: tileRow)
+                    if tile?.name == "champion-select-road" {
+                        self.player?.position = newPosition
+                    }
+                }
+            }
+            
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
