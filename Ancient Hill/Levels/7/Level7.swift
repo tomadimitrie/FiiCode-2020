2ec1edd2dbfe2d581b696cee719ab21935f61f63
Dimitrie-Toma Furdui <tomadimitrie@icloud.com>
2020-05-02 20:06:56 +0300

    added up to level 8 & main screen

---
+++ Ancient Hill/Levels/7/Level7.swift
@@ -0,0 +1,9 @@
+//
+//  Level7.swift
+//  Ancient Hill
+//
+//  Created by Dimitrie-Toma Furdui on 02/05/2020.
+//  Copyright © 2020 Green Meerkats of Romania. All rights reserved.
+//
+
+import Foundation
